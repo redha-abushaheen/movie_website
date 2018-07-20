f7419baa7dad931a42b29aa3ece773963dcd337a
stone30 <newn1988@gmail.com>
2018-07-20 18:11:46 +0300

    checking

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,52 +1,43 @@
 #!/usr/bin/env python
-"""class constructor where we will place our movies"""
+"""class where we will place our movies"""
+
+
 import media
 import fresh_tomatoes
 
 
-#A list of movies
-toystory=media.Movie("Toy Story",
-	"Toy Story is about the 'secret life of toys'",
-	"https://goo.gl/XqPqLT",
-	"https://www.youtube.com/watch?v=4KPTXpQehio")
+# A list of movies
+toy_story = media.Movie("Toy Story",
+                        "Toy Story is about the 'secret life of toys'",
+                        "https://goo.gl/XqPqLT",
+                        "https://www.youtube.com/watch?v=4KPTXpQehio")
 
-                  
+avatar = media.Movie("Avatar",
+                     "A marine turn into an alien",
+                     "https://goo.gl/3WcdY8",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-#print (toystory.storyline)
+matrix = media.Movie("The Matrix",
+                     "Neo (-- What is the Matrix?",
+                     "https://goo.gl/WRZBUz",
+                     "https://www.youtube.com/watch?v=vKQi3bBA1y8")
 
-avatar=media.Movie("Avatar",
-	"A marine turn into an alien",
-"https://goo.gl/3WcdY8",
-"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+gladiator = media.Movie("Gladiator",
+                        "A man coming back",
+                        "https://goo.gl/qPivN8",
+                        "https://youtu.be/owK1qxDselE")
 
-matrix=media.Movie("The Matrix",
-	"Neo (-- What is the Matrix?",
-"https://goo.gl/WRZBUz",
-"https://www.youtube.com/watch?v=vKQi3bBA1y8")
+taken = media.Movie("Taken",
+                    "A Father girl kidnapped",
+                    "https://goo.gl/6c1fYs",
+                    "https://youtu.be/kZ02_Uzf7So")
 
-gladiator=media.Movie("Gladiator",
-	"A man coming back",
-	"https://goo.gl/qPivN8",
-	"https://youtu.be/owK1qxDselE")
-
-taken=media.Movie("Taken",
-	"A Father girl kidnapped",
-	"https://goo.gl/6c1fYs",
-	"https://youtu.be/kZ02_Uzf7So")
-
-braveheart=media.Movie("Taken",
-	"Epic war heart",
-	"https://goo.gl/93QUfP",
-	"https://youtu.be/nMft5QDOHek")
+brave_heart = media.Movie("BraveHeart",
+                          "Epic war heart",
+                          "https://goo.gl/93QUfP",
+                          "https://youtu.be/nMft5QDOHek")
 # an array with the movies listed
-xyz= [toystory,avatar,matrix,gladiator,taken,braveheart]
+listofmovies = [toy_story, avatar, matrix, gladiator, taken, brave_heart]
 
 
-fresh_tomatoes.open_movies_page(xyz)
-#print (matrix.storyline)
-#print (avatar.trailer_url)
-#matrix.show_trailer()
-
-print media.Movie.__doc__
-print media.Movie.__name__
-print media.Movie.__module__
+fresh_tomatoes.open_movies_page(listofmovies)
--- media.py
+++ media.py
@@ -1,14 +1,22 @@
 import webbrowser
 
+
 class Movie():
     """This class provide a way to store movie variables"""
-    def __init__(self, movie_title,movie_storyline,poster_image,trailer):
-        self.title=movie_title
-        self.storyline=movie_storyline
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer):
+        """
+        set up movie objects
+        movie_title:string repesenting movie title
+        movie_storyline:brief plot synopsis
+        poster_image:movie image
+        trailer: the triler for movie
+        """
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer
 
     def show_trailer(self):
+        """open trailer in the browser"""
         webbrowser.open(self.trailer_youtube_url)
-#        super([object Object], self).__init__()
-#        self.arg = arg
+
